f0e2e272e34bfeae04f8e93d9177c807c9652059
lirugo <roostik21@gmail.com>
2019-12-11 16:56:16 +0200

    Mockup for table calc history

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -11,7 +11,11 @@
                 outlined
         >
           <v-card-text>
-
+            <div class="text-right">
+              <small>
+                *Use dot as decimal separator
+              </small>
+            </div>
             <v-text-field v-model="operandA" type="number" class="mb-3" outlined label="Operand A"/>
             <v-text-field v-model="operandB" type="number" class="mb-3" outlined label="Operand B"/>
             <v-select v-model="selectedAction"
@@ -30,6 +34,27 @@
           <v-btn text large block @click="calculate()">Calculate</v-btn>
         </v-card>
       </v-col>
+
+      <v-col>
+        <v-card
+                class="mx-auto"
+                max-width="700"
+                outlined
+        >
+          <v-card-text>
+            <div class="text-right">
+              <small>
+                *Data will release after page refresh
+              </small>
+            </div>
+            <v-data-table
+                    :headers="calcHeaders"
+                    :items="calcHistory"
+                    :items-per-page="5"
+            />
+          </v-card-text>
+        </v-card>
+      </v-col>
     </v-row>
   </v-container>
 </template>
@@ -40,16 +65,30 @@
       operandA: null,
       operandB: null,
       selectedAction: null,
-      actions: [
-              { action: '+', title: '+ (sum)' },
-              { action: '/', title: '/ (divide)' },
-              { action: '%', title: '% (remainder of a division)' },
-              { action: '>', title: 'Highest prime number between A and B' },
-      ],
       result: null,
+      actions: [
+        { action: '+', title: '+ (sum)' },
+        { action: '/', title: '/ (divide)' },
+        { action: '%', title: '% (remainder of a division)' },
+        { action: '>', title: 'Highest prime number between A and B' },
+      ],
+
+      calcHeaders: [
+        { text: '#', value: 'id', align: 'center',},
+        { text: 'Operand A', value: 'operandA', align: 'right',},
+        { text: 'Action', value: 'action', align: 'center',},
+        { text: 'Operand B', value: 'operandB' },
+        {
+          text: 'Result',
+          align: 'left',
+          value: 'result',
+        },
+      ],
+      calcHistory: [],
     }),
     methods: {
       calculate(){
+        //Get result
         switch (this.selectedAction) {
           case '+' : this.result = this.sum(this.operandA, this.operandB); break
           case '/' : this.result = this.divide(this.operandA, this.operandB); break
@@ -57,6 +96,23 @@
           case '>' : this.result = this.highestNumber(this.operandA, this.operandB); break
           default : this.result = 'Select action'; break
         }
+
+        //Save to history
+        this.saveToHistory()
+      },
+      saveToHistory(){
+        let actionTitle
+        this.actions.forEach(a => {
+          a.action === this.selectedAction ? actionTitle = a.title : ''
+        })
+
+        this.calcHistory.unshift({
+          id: this.calcHistory.length + 1,
+          operandA: this.operandA,
+          operandB: this.operandB,
+          action: actionTitle,
+          result: this.result,
+        })
       },
       sum(a, b){
         return Number(a) + Number(b)
